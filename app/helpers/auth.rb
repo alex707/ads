9b605315639e05c8f35c0beaf74773d4efe45fa5
alex707 <alex.grigorenko2942@gmail.com>
2021-04-13 22:36:31 +0300

    added producer for detecting user_id by token from auth-microservice

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -12,7 +12,7 @@ module Auth
   private
 
   def auth_service
-    @auth_service ||= AuthService::Client.new
+    @auth_service ||= AuthService::Client.fetch
   end
 
   def matched_token
--- app/lib/auth_service/client.rb
+++ app/lib/auth_service/client.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 require 'dry/initializer'
 require_relative 'api'
 
@@ -6,17 +7,65 @@ module AuthService
     extend Dry::Initializer[undefined: false]
     include Api
 
-    option :url, default: proc { 'http://localhost:3010/v1' }
-    option :connection, default: proc { build_connection }
+    attr_writer :user_id
+
+    option :queue, default: proc { create_queue }
+    option :reply_queue, default: proc { create_reply_queue }
+    option :lock, default: proc { Mutex.new }
+    option :condition, default: proc { ConditionVariable.new }
+    option :user_id, optional: true
+
+    def self.fetch
+      Thread.current['auth_service.rpc_client'] ||= new.start
+    end
+
+    def start
+      @reply_queue.subscribe do |delivery_info, properties, payload|
+        if properties[:correlation_id] == @correlation_id
+          Thread.current['user_id'] = JSON.parse(payload)['user_id']
+
+          @lock.synchronize do
+            @user_id = Thread.current['user_id']
+            @condition.signal
+          end
+        end
+      end
+
+      self
+    end
 
     private
 
-    def build_connection
-      Faraday.new(@url) do |conn|
-        conn.request :json
-        conn.response :json, content_type: /\bjson$/
-        conn.adapter Faraday.default_adapter
+    attr_writer :correlation_id
+
+    def create_queue
+      channel = RabbitMq.channel
+      channel.queue('auth', durable: true)
+    end
+
+    def create_reply_queue
+      channel = RabbitMq.channel
+      channel.queue('auth-reply-to', durable: true)
+    end
+
+    def publish(payload, opts = {})
+      @lock.synchronize do
+        self.correlation_id = SecureRandom.uuid
+
+        @queue.publish(
+          payload,
+          opts.merge(
+            persistent: true,
+            app_id: 'ads',
+            correlation_id: @correlation_id,
+            reply_to: @reply_queue.name
+          )
+        )
+
+        @condition.wait(@lock)
       end
+
+      @user_id
     end
   end
 end
