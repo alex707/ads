eb2810ff7142cb4ba7e4634daa1ae2ba57009680
alex707 <alex.grigorenko2942@gmail.com>
2021-05-03 14:49:47 +0300

    Revert "rpc for detecting user_id by token"

--- app/lib/auth_service/client.rb
+++ app/lib/auth_service/client.rb
@@ -1,8 +1,3 @@
-# token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiNmFiZGQ0ZjEtMDBkNC00NWE3LThhMGEtMzU2NGMzOThmOWUyIn0.k5OAp3OpX4PQhFPMFzt8VOStrn9xOyBzylGU6Hhzecw'
-# auth_client = AuthService::Client.fetch
-# user_id = auth_client.auth(token)
-
-require 'securerandom'
 require 'dry/initializer'
 require_relative 'api'
 
@@ -11,79 +6,17 @@ module AuthService
     extend Dry::Initializer[undefined: false]
     include Api
 
-    attr_writer :user_id
-
-    option :queue, default: proc { create_queue }
-    option :reply_user_id_queue, default: proc { create_reply_user_id_queue }
-    option :lock, default: proc { Mutex.new }
-    option :condition, default: proc { ConditionVariable.new }
-    option :user_id, optional: true
-    option :exchange, default: proc { create_exchange }
-
-    def self.fetch
-      Thread.current['auth_service.rpc_client'] ||= new.start
-    end
-
-    def start
-      @reply_user_id_queue.subscribe do |delivery_info, properties, payload|
-        if properties[:correlation_id] == @correlation_id
-          Thread.current['user_id'] = JSON.parse(payload)['user_id']
-
-          @lock.synchronize do
-            @user_id = Thread.current['user_id']
-            @condition.signal
-          end
-
-          exchange.publish(
-            '',
-            routing_key: 'auth',
-            correlation_id: properties.correlation_id,
-            user_id: @user_id,
-            app_id: 'auth',
-          )
-        end
-      end
-
-      self
-    end
+    option :url, default: proc { 'http://localhost:3010/v1' }
+    option :connection, default: proc { build_connection }
 
     private
 
-    attr_writer :correlation_id
-
-    def create_queue
-      channel = RabbitMq.channel
-      channel.queue('auth', durable: true)
-    end
-
-    def create_reply_user_id_queue
-      channel = RabbitMq.channel
-      channel.queue('auth-user-id', durable: true)
-    end
-
-    def create_exchange
-      channel = RabbitMq.channel
-      channel.default_exchange
-    end
-
-    def publish(payload, opts = {})
-      @lock.synchronize do
-        self.correlation_id = SecureRandom.uuid
-
-        @queue.publish(
-          payload,
-          opts.merge(
-            persistent: true,
-            app_id: 'ads',
-            correlation_id: @correlation_id,
-            reply_to: @reply_user_id_queue.name
-          )
-        )
-
-        @condition.wait(@lock)
+    def build_connection
+      Faraday.new(@url) do |conn|
+        conn.request :json
+        conn.response :json, content_type: /\bjson$/
+        conn.adapter Faraday.default_adapter
       end
-
-      @user_id
     end
   end
 end
--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -10,7 +10,7 @@ queue.subscribe do |delivery_info, properties, payload|
 
   exchange.publish(
     '',
-    routing_key: properties.reply_to, # сюда придёт дефолтная очередь
+    routring_key: properties.reply_to, # дефолтная очередь
     correlation_id: properties.correlation_id
   )
 end
